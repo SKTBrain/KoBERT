613004641bbc801ac44375d2f5a046c8931803f6
Heewon Jeon(gogamza) <gogamza@gmail.com>
2019-12-19 16:19:29 +0900

    Update tokenizer
    #11

--- kobert/utils.py
+++ kobert/utils.py
@@ -27,7 +27,7 @@ onnx_kobert = {
 
 tokenizer = {
     'url':
-    'https://kobert.blob.core.windows.net/models/kobert/tokenizer/kobert_news_wiki_ko_cased-1087f8699e.spiece',
+    'https://kobert.blob.core.windows.net/models/kobert/tokenizer/kobert_news_wiki_ko_cased-ae5711deb3.spiece',
     'fname': 'kobert_news_wiki_ko_cased-1087f8699e.spiece',
     'chksum': '1087f8699e'
 }
