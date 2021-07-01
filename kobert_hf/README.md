98b92cd482f798cd43da4afd4ae4fdbc5a4d9e47
seujung <digit82@gmail.com>
2021-07-01 16:42:19 +0900

    update huggingface based tokenizer

---
+++ kobert_hf/README.md
@@ -0,0 +1,70 @@
+
+
+<!-- @import "[TOC]" {cmd="toc" depthFrom=1 depthTo=6 orderedList=false} -->
+
+<!-- code_chunk_output -->
+
+- [Korean BERT pre-trained cased (KoBERT) for Huggingface Transformers](#korean-bert-pre-trained-cased-kobert-for-huggingface-transformers)
+  - [Requirements](#requirements)
+  - [How to install](#how-to-install)
+- [Tokenizer](#tokenizer)
+- [Model](#model)
+- [License](#license)
+
+<!-- /code_chunk_output -->
+
+---
+
+### Korean BERT pre-trained cased (KoBERT) for Huggingface Transformers
+
+KoBERT를 Huggingface.co 기반으로 사용할 수 있게 Wrapping 작업을 수행하였습니다.
+
+
+#### Requirements
+
+* Python >= 3.6
+* PyTorch >= 1.8.1
+* transformers >= 4.8.2
+
+#### How to install
+
+```sh
+git clone https://github.com/SKTBrain/KoBERT.git
+cd KoBERT/kobert_hf
+pip install -r requirements.txt
+python setup.py install
+```
+
+---
+
+### Tokenizer
+
+[XLNetTokenizer](https://github.com/huggingface/transformers/blob/master/src/transformers/models/xlnet/tokenization_xlnet.py)를 활용하여 Wrapping 작업을 진행하였습니다.
+
+기존 Tokenizer와 동일하게 사전 크기는 8,002개 입니다.
+
+```python
+> from kobert_tokenizer import KoBERTTokenizer
+> tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+> tokenizer.encode("한국어 모델을 공유합니다.")
+[2, 4958, 6855, 2046, 7088, 1050, 7843, 54, 3]
+
+```
+
+### Model
+```python
+> import torch
+> from transformers import BertModel
+> model = BertModel.from_pretrained('skt/kobert-base-v1')
+> text = "한국어 모델을 공유합니다."
+> inputs = tokenizer.batch_encode_plus([text])
+> out = model(input_ids = torch.tensor(inputs['input_ids']),
+              attention_mask = torch.tensor(inputs['attention_mask']))
+> out.pooler_output.shape
+torch.Size([1, 768])
+
+```
+
+### License
+
+`KoBERT`는 Apache-2.0 라이선스 하에 공개되어 있습니다. 모델 및 코드를 사용할 경우 라이선스 내용을 준수해주세요. 라이선스 전문은 `LICENSE` 파일에서 확인하실 수 있습니다.
---
+++ kobert_hf/kobert_tokenizer/kobert_tokenizer.py
@@ -0,0 +1,65 @@
+from typing import Any, Dict, List, Optional, Tuple
+from transformers.tokenization_utils import AddedToken
+from transformers import XLNetTokenizer
+
+class KoBERTTokenizer(XLNetTokenizer):
+    def __init__(
+        self,
+        vocab_file,
+        do_lower_case=False,
+        remove_space=True,
+        keep_accents=False,
+        bos_token="[CLS]",
+        eos_token="[SEP]",
+        unk_token="[UNK]",
+        sep_token="[SEP]",
+        pad_token="[PAD]",
+        cls_token="[CLS]",
+        mask_token="[MASK]",
+        additional_special_tokens=None,
+        sp_model_kwargs: Optional[Dict[str, Any]] = None,
+        **kwargs
+    ) -> None:
+        # Mask token behave like a normal word, i.e. include the space before it
+        mask_token = AddedToken(mask_token, lstrip=True, rstrip=False) if isinstance(mask_token, str) else mask_token
+
+        self.sp_model_kwargs = {} if sp_model_kwargs is None else sp_model_kwargs
+
+        super().__init__(
+            vocab_file,
+            do_lower_case=do_lower_case,
+            remove_space=remove_space,
+            keep_accents=keep_accents,
+            bos_token=bos_token,
+            eos_token=eos_token,
+            unk_token=unk_token,
+            sep_token=sep_token,
+            pad_token=pad_token,
+            cls_token=cls_token,
+            mask_token=mask_token,
+            additional_special_tokens=additional_special_tokens,
+            sp_model_kwargs=self.sp_model_kwargs,
+            **kwargs,
+        )
+    
+    def build_inputs_with_special_tokens(
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
+        """
+        Build model inputs from a sequence or a pair of sequence for sequence classification tasks by concatenating and
+        adding special tokens. An XLNet sequence has the following format:
+        - single sequence: ``X <sep> <cls>``
+        - pair of sequences: ``A <sep> B <sep> <cls>``
+        Args:
+            token_ids_0 (:obj:`List[int]`):
+                List of IDs to which the special tokens will be added.
+            token_ids_1 (:obj:`List[int]`, `optional`):
+                Optional second list of IDs for sequence pairs.
+        Returns:
+            :obj:`List[int]`: List of `input IDs <../glossary.html#input-ids>`__ with the appropriate special tokens.
+        """
+        sep = [self.sep_token_id]
+        cls = [self.cls_token_id]
+        if token_ids_1 is None:
+            return cls + token_ids_0 + sep
+        return cls + token_ids_0 + sep + token_ids_1 + sep
