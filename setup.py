42469e9fb7706d98743313d2aa9969a4435900d9
Bage <bage79@gmail.com>
2022-01-11 12:50:59 +0900

    🐛 fix `No module named 'kobert.utils'`

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def install_requires():
@@ -7,18 +7,15 @@ def install_requires():
         install_requires = [line for line in lines]
         return install_requires
 
-
 setup(
     name="kobert",
-    version="0.2.1",
+    version="0.2.2",
     url="https://github.com/SKTBrain/KoBERT",
     license="Apache-2.0",
     author="Heewon Jeon",
     author_email="madjakarta@gmail.com",
     description="Korean BERT pre-trained cased (KoBERT) ",
-    packages=[
-        "kobert",
-    ],
+    packages=find_packages(),
     long_description=open("README.md", encoding="utf-8").read(),
     zip_safe=False,
     include_package_data=True,
